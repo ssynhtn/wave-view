5b1c06086f72f41720776c579d7d08a77e5a917b
huangtongnao <huangtongnao@gmail.com>
2018-04-12 18:02:44 +0800

    Initial commit

---
+++ app/src/main/java/com/ssynhtn/waveview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ssynhtn.waveview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private WaveView waveView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        waveView = findViewById(R.id.wave_three);
+        waveView.addDefaultWaves(2, 1);
+
+        waveView.startAnimation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        waveView.resumeAnimation();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        waveView.pauseAnimation();
+    }
+}
